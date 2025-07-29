c3235d8e97eba32032f5018c476ec5d7390a7166
poornimaraj0307 <poornimaraj0307@gmail.com>
2025-07-29 10:06:16 +0530

    Add files via upload

---
+++ py3.py
@@ -0,0 +1,12 @@
+print("Government Of TamilNadu")
+print("Electricty Boarde")
+print("------------------------")
+no1=input("Entry the EB-no:")
+n02=input("Entry the costumer Name:")
+pre=int(input("Reading for Previous Month:"))
+cur=int(input("Reading for Current Month:"))
+Total=cur-pre
+print("Total Unit Consule:",Total)
+Amount=Total*5
+print("Amount to be paid:",Amount)
+        
