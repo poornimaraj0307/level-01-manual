4e433d61a7b03a516d878df051bf2f6ad147a2a5
poornimaraj0307 <poornimaraj0307@gmail.com>
2025-07-29 09:49:32 +0530

    Add files via upload

---
+++ py1.py
@@ -0,0 +1,15 @@
+print("ARITHEMATIC OPERATIONS")
+print("------------------------")
+a=int(input("Entry the first number:"))
+b=int(input("Entry the second number:"))
+print("result")
+c=a+b
+print("add:",c)
+c=a-b
+print("sub:",c)
+c=a*b
+print("mult:",c)
+c=a/b
+print("divi:",c)
+c=a%b
+print("modul:",c)
---
+++ py2.py
@@ -0,0 +1,14 @@
+print("Aravindha University")
+print("Avinashi Road,Coimbatore")
+print("----------------------------")
+print("Student Mark List")
+print("----------------------")
+no=input("Entry the Register No:")
+name=input("Entry the Name:")
+sub1=int(input("Python mark:"))
+sub2=int(input("DBMS mark:"))
+sub3=int(input("Accounting mark:"))
+total=sub1+sub2+sub3
+print("Total mark:",total)
+ave=total/3
+print("Average mark:",ave)
